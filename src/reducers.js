2df01b970daabff053e9ce311207f7eb0e9422bc
farazkhan2373 <136571344+farazkhan2373@users.noreply.github.com>
2023-10-08 11:43:48 +0530

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,25 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      const newUser = {
+        id: uuidv4(),
+        name: action.name
+      };
+      action.setBalance(action.balance + 10);
+      return [...state, newUser];
+
+    case "fuel":
+      const currentBalance = action.balance;
+      if (currentBalance >= 50) {
+        action.setBalance(currentBalance - 50);
+      }
+      return state;
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
